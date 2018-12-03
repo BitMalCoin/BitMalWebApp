bbe4158392b7b0500aeeec441d72bc7e08a4560b
András T. Takács <maurjin@gmail.com>
2018-12-03 18:03:23 +0100

    WIP: bitmal transactions component

---
+++ components/user/BitmalTransactions.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="bitmal-transactions">
+    <div
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      class="bitmal-transaction">
+      <div class="first-row">
+        <div :class="['iconholder', {minus: transaction.amount < 0}]">
+          <i class="el-icon-back"/>
+        </div>
+        {{ Math.abs(transaction.amount) }}
+        <span>BMC</span>
+      </div>
+      <div class="second-row">
+        <div class="left">{{ transaction.info }}</div>
+        <div class="right">{{ transaction.when }}</div>
+      </div>
+      <div class="hash">
+        Transaction hash: {{ transaction.hash }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+      transactions: [
+        {
+          id: 'asdf1',
+          amount: 100,
+          info: 'by Csaba Makra for your project “Renovate school for Children””',
+          hash: '0xsdfer84568945698ewrlertiertjlert45040564telfgl4564324aayxccc01233xgdfgdfgt',
+          when: '2018/06/14'
+        },
+        {
+          id: 'asdf2',
+          amount: -15,
+          info: 'by Csaba Makra for your project “Renovate school for Children””',
+          hash: '0xsdfer84568945698ewrlertiertjlert45040564telfgl4564324aayxccc01233xgdfgdfgt',
+          when: '2018/06/13'
+        },
+        {
+          id: 'asdf3',
+          amount: 12,
+          info: 'by Csaba Makra for your project “Renovate school for Children””',
+          hash: '0xsdfer84568945698ewrlertiertjlert45040564telfgl4564324aayxccc01233xgdfgdfgt',
+          when: '2018/06/12'
+        },
+        {
+          id: 'asdf4',
+          amount: -8,
+          info: 'by Csaba Makra for your project “Renovate school for Children””',
+          hash: '0xsdfer84568945698ewrlertiertjlert45040564telfgl4564324aayxccc01233xgdfgdfgt',
+          when: '2018/06/11'
+        }
+      ]
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+@import '../../assets/style/main.less';
+
+.bitmal-transactions {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .bitmal-transaction {
+    max-width: 800px;
+    height: 130px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    .first-row {
+      .bold();
+      font-size: 20px;
+      line-height: 24px;
+
+      .iconholder {
+        display: inline-block;
+        width: 24px;
+        height: 24px;
+        line-height: 24px;
+        text-align: center;
+        background-color: #54BB5C;
+        transform: rotate(90deg);
+
+        i {
+          color: white;
+          .bold();
+          font-size: 16px;
+        }
+
+        &.minus {
+          background-color: #F15B4A;
+          transform: rotate(270deg);
+        }
+      }
+
+      span {
+        color: @text-light;
+        font-size: 15px;
+      }
+    }
+  }
+}
+
+</style>
--- pages/user/transactions.vue
+++ pages/user/transactions.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="transactions-tabs">
     <el-tabs type="card">
+
       <el-tab-pane label="Bitmal wallet">
         <div class="transactions-inner wallet">
           <el-row
@@ -15,7 +16,29 @@
           </el-row>
         </div>
       </el-tab-pane>
-      <el-tab-pane label="Bitmal transactions">Bitmal transactions component...</el-tab-pane>
+
+      <el-tab-pane label="Bitmal transactions">
+        <div class="bm-trans-tab">
+          <div class="upper">
+            Your Bitmal transactions sorted by
+            <el-select
+              v-model="value"
+              class="bitmal-input bitmal-input-filter"
+              popper-class="bitmal-input-popper">
+              <el-option
+                v-for="option in options"
+                :key="option.value"
+                :label="option.value"
+                :value="option.value"/>
+            </el-select>
+          </div>
+
+          <div class="lower">
+            <bitmal-transactions />
+          </div>
+        </div>
+      </el-tab-pane>
+
       <el-tab-pane label="USD transactions">USD transactions component...</el-tab-pane>
     </el-tabs>
   </div>
@@ -24,9 +47,20 @@
 <script>
 import WalletBalance from '../../components/user/WalletBalance'
 import StripeBuy from '../../components/user/StripeBuy'
+import BitmalTransactions from '../../components/user/BitmalTransactions'
 
 export default {
-  components: { WalletBalance, StripeBuy },
+  components: { WalletBalance, StripeBuy, BitmalTransactions },
+
+  data () {
+    return {
+      options: [
+        { value: 'Newest' },
+        { value: 'Oldest' }
+      ],
+      value: {}
+    }
+  },
 
   computed: {
     fullOnXs () {
@@ -38,24 +72,49 @@ export default {
         xl: 12
       }[this.$mq]
     }
-
   }
 }
 </script>
 
 <style lang="less" scoped>
 @import '../../assets/style/main.less';
-.transactions-inner {
-  display: flex;
-  justify-content: center;
+.transactions-tabs {
 
-  &.wallet {
-    margin-top: 70px;
+  .transactions-inner {
+    display: flex;
+    justify-content: center;
+
+    &.wallet {
+      margin-top: 70px;
+    }
+
+    .eighth {
+      max-width: 840px;
+    }
+
   }
 
-  .eighth {
-    max-width: 840px;
+  .bm-trans-tab {
+    .upper {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100px;
+      border-bottom: 1px solid @ui;
+
+      .bitmal-input-filter {
+        margin-left: 12px;
+        max-width: 170px;
+      }
+    }
+
+    .lower {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
   }
+
 }
 
 </style>
